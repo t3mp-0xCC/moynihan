71231a7adf04f2edda7541825c3da668b972479a
t3mp-0xCC <40132263+t3mp-0xCC@users.noreply.github.com>
2022-09-22 22:23:25 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use futures::{
+    channel::mpsc::{channel, Receiver},
+    SinkExt, StreamExt,
+};
+use notify::{
+    Event,
+    RecommendedWatcher,
+    RecursiveMode,
+    Watcher,
+    Config, EventKind,
+    event::{ModifyKind, MetadataKind},
+};
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+use std::fs::File;
+
+static LOG_PATH: &str = "/var/log/nginx/error.log";
+
+fn main() {
+    let log_path = Path::new(LOG_PATH);
+    futures::executor::block_on(async{
+        if let Err(e) = async_watch(log_path).await {
+            println!("error: {:?}", e);
+        }
+    });
+}
+
+fn get_latest_log(log_path: &Path) -> String {
+    let log_file= BufReader::new(File::open(log_path).unwrap());
+    let log_iter = log_file.lines().map(|l| l.unwrap());
+    let last_log = match log_iter.last() {
+        Some(s) => s,
+        None => panic!("Invalid log file"),
+    };
+
+    last_log
+}
+
+fn event_handler(event: Event) {
+    if event.kind == EventKind::Modify(ModifyKind::Metadata(MetadataKind::Any)) || event.kind == EventKind::Modify(ModifyKind::Any) {
+        let last_log = get_latest_log(Path::new(LOG_PATH));
+        println!("{}", last_log);
+    }
+}
+
+// https://github.com/notify-rs/notify/blob/main/examples/async_monitor.rs
+fn async_watcher() -> notify::Result<(RecommendedWatcher, Receiver<notify::Result<Event>>)> {
+    let (mut tx, rx) = channel(1);
+    let watcher = RecommendedWatcher::new(move |res| {
+        futures::executor::block_on(async {
+            tx.send(res).await.unwrap();
+        })
+    }, Config::default())?;
+
+    Ok((watcher, rx))
+}
+
+async fn async_watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
+    let (mut watcher, mut rx) = async_watcher()?;
+    watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
+
+    while let Some(res) = rx.next().await {
+        match res {
+            Ok(event) => event_handler(event),
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    static LOG_PATH: &str = "./test/test.log";
+    #[test]
+    fn get_latest_log_test() {
+        let last_log = get_latest_log(Path::new(LOG_PATH));
+        assert_eq!(last_log, "2022/09/22 20:06:54 [error] 1036243#1036243: *3757626 no live upstreams while connecting to upstream, client: 192.168.11.4, server: , request: \"GET /piyo HTTP/1.1\", upstream: \"http://localhost/\", host: \"192.168.11.1\"")
+    }
+}
